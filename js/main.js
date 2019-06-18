fb3121cc4efe705c81b45f95cbfcb511656e2627
hiira <littlebony.hii@gmail.com>
2019-06-18 14:27:55 +0900

    first_commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', ()=>{
+         const results=['大吉', '中吉', '凶', '末吉'];
+         const results=['大吉','大吉','大吉','大吉',,];
+         const n = Math.floor(Math.random()*results.length);
+
+          btn.textContent = results[n];
+    });
+
+    btn.addEventListener('mousedown', ()=>{
+     btn.classList.add('pressed');
+});
+
+     btn.addEventListener('mouseup', ()=>{
+     btn.classList.remove('pressed');
+});
+
+
+    }
